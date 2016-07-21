b69406835f0caedc827aeda11b47f1d32c99a947
Tyler Akins <fidian@rumkin.com>
2016-07-21 09:34:36 -0500

    Split test into three

---
+++ tests/source-bad-file.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd "${0%/*}"
+echo "Do not display this" | ../mo --source=invalid 2>&1
+
+if [[ $? -ne 1 ]]; then
+    echo "Did not return 1"
+fi
--- tests/source.sh
+++ tests/source.sh
@@ -8,10 +8,3 @@ cat <<EOF | ../mo --source=source.vars
 {{/ARR}}
 {{ASSOC_ARR.a}} {{ASSOC_ARR.b}}
 EOF
-
-
-# Prints the string should mo NOT fail. Meaning that the output will not match
-# tests/source.expected and therefore the test will fail.
-
-../mo --source=a/non/existent/file files >/dev/null 2>&1
-[[ "$?" -ne 1 ]] && echo "mo accepted a non existent file"
